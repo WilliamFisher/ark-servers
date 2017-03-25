5d05dbb2176e495a4899b5736f0c067f7c266d63
William Fisher <ashton0312@gmail.com>
2017-03-25 16:32:51 -0400

    Created Server model and migration

---
+++ database/migrations/2017_03_25_172915_create_servers_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateServersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('servers', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('user_id');
+            $table->string('name', 16);
+            $table->string('map');
+            $table->text('description');
+            $table->boolean('is_pvp');
+            $table->float('xp_rate');
+            $table->float('gather_rate');
+            $table->float('tame_speed');
+            $table->float('difficulty')
+            $table->date('last_wipe');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('servers');
+    }
+}
