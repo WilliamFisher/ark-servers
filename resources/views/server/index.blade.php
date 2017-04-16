74bd04121e8c54d6c5940ac74616366aabfae060
William Fisher <ashton0312@gmail.com>
2017-04-16 11:31:51 -0400

    Made table responsive

--- resources/views/server/index.blade.php
+++ resources/views/server/index.blade.php
@@ -8,30 +8,36 @@
                 <div class="panel-heading">All Servers</div>
 
                 <div class="panel-body">
-                  <table class="table">
-                    <tr>
-                      <th>Name</th>
-                      <th>Description</th>
-                      <th>Map</th>
-                      <th>Platform</th>
-                      <th>PvP</th>
-                      <th>PvE</th>
-                      <th>XP Rate</th>
-                      <th>Actions</th>
-                    </tr>
-                    <tr>
-                      @foreach($servers as $server)
-                      <td>{{ $server->name }}</td>
-                      <td>{{ $server->description }}</td>
-                      <td>{{ $server->map }}</td>
-                      <td>{{ $server->platform }}</td>
-                      <td>{{ $server->is_pvp }}</td>
-                      <td>{{ $server->is_pve }}</td>
-                      <td>{{ $server->xp_rate }}</td>
-                      <td><a class="btn btn-sm btn-primary" href="{{ url('servers/' . $server->id) }}">View</a></td>
-                      @endforeach
-                    </tr>
-                  </table>
+                  <div class="table-responsive">
+                    <table class="table">
+                      <thead>
+                        <tr>
+                          <th>Name</th>
+                          <th>Description</th>
+                          <th>Map</th>
+                          <th>Platform</th>
+                          <th>PvP</th>
+                          <th>PvE</th>
+                          <th>XP Rate</th>
+                          <th>Actions</th>
+                        </tr>
+                      </thead>
+                      <tbody>
+                        @foreach($servers as $server)
+                        <tr>
+                          <td>{{ $server->name }}</td>
+                          <td>{{ $server->description }}</td>
+                          <td>{{ $server->map }}</td>
+                          <td>{{ $server->platform }}</td>
+                          <td>{{ $server->is_pvp }}</td>
+                          <td>{{ $server->is_pve }}</td>
+                          <td>{{ $server->xp_rate }}</td>
+                          <td><a class="btn btn-sm btn-primary" href="{{ url('servers/' . $server->id) }}">View</a></td>
+                        </tr>
+                        @endforeach
+                      </tbody>
+                    </table>
+                  </div>
                 </div>
             </div>
         </div>
