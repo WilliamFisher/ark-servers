6b837a90f6d90a6232a95975401814f73a3c085e
William Fisher <ashton0312@gmail.com>
2017-05-08 11:21:24 -0400

    Added Dashboard functionality

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,12 +3,41 @@
 @section('content')
 <div class="container">
     <div class="row">
-        <div class="col-md-8 col-md-offset-2">
+        <div class="col-md-10 col-md-offset-1">
             <div class="panel panel-default">
-                <div class="panel-heading">Dashboard</div>
+                <div class="panel-heading">My Servers</div>
 
                 <div class="panel-body">
-                    You are logged in!
+                  <div class="table-responsive">
+                    <table class="table">
+                      <thead>
+                        <tr>
+                          <th>Name</th>
+                          <th>Map</th>
+                          <th>Platform</th>
+                          <th>PvP</th>
+                          <th>PvE</th>
+                          <th>XP Rate</th>
+                          <th>Actions</th>
+                        </tr>
+                      </thead>
+                      <tbody>
+                        @foreach($servers as $server)
+                        <tr>
+                          <td>{{ $server->name }}</td>
+                          <td>{{ $server->map }}</td>
+                          <td>{{ $server->platform }}</td>
+                          <td>{{ $server->is_pvp }}</td>
+                          <td>{{ $server->is_pve }}</td>
+                          <td>{{ $server->xp_rate }}</td>
+                          <td>
+                            <a class="btn btn-sm btn-primary" href="{{ url('servers/' . $server->id) }}">View</a>
+                          </td>
+                        </tr>
+                        @endforeach
+                      </tbody>
+                    </table>
+                  </div>
                 </div>
             </div>
         </div>
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -43,7 +43,8 @@
                 <div class="collapse navbar-collapse" id="app-navbar-collapse">
                     <!-- Left Side Of Navbar -->
                     <ul class="nav navbar-nav">
-                        <li><a href="{{ url('servers') }}">View Servers</a>
+                        <li><a href="{{ url('servers') }}">View Servers</a></li>
+                        <li><a href="{{ url('home') }}">My Servers</a></li>
                     </ul>
 
                     <!-- Right Side Of Navbar -->
