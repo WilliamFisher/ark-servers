6ac15ee13c5ecdb2fb773b0b9e9ba6869b13fffd
William Fisher <ashton0312@gmail.com>
2017-07-23 00:01:52 -0400

    Created CommentsController

---
+++ app/Http/Controllers/CommentsController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+use App\Server;
+use App\Comment;
+use Auth;
+
+class CommentsController extends Controller
+{
+    public function store(Server $server)
+    {
+      $this->validate(request(), ['body' => 'required|min:2']);
+
+      $comment = new Comment;
+      $comment->user_id = Auth::user()->id;
+      $comment->server_id = $server->id;
+      $comment->body = request('body');
+
+      $comment->save();
+
+      return back();
+    }
+}
