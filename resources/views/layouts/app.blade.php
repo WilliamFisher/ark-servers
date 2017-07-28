d9af67206d654625a476ac2c2240da547f7014ea
William Fisher <ashton0312@gmail.com>
2017-07-28 01:06:09 -0400

    Changed Laravel defaults

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -8,7 +8,7 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>{{ config('app.name', 'Laravel') }}</title>
+    <title>{{ config('app.name', 'Ark Servers') }}</title>
 
     @yield('headScripts')
 
@@ -38,7 +38,7 @@
 
                     <!-- Branding Image -->
                     <a class="navbar-brand" href="{{ url('/servers') }}">
-                        {{ config('app.name', 'Laravel') }}
+                        {{ config('app.name', 'Ark Servers') }}
                     </a>
                 </div>
 
