0f57ff5a343054115b6506d1661f01924712c168
William Fisher <ashton0312@gmail.com>
2017-08-22 22:10:20 -0400

    Quick and dirty fix for error logging ip

--- app/SocialAccountService.php
+++ app/SocialAccountService.php
@@ -32,6 +32,7 @@ class SocialAccountService
                     'email' => $providerUser->getEmail(),
                     'name' => $providerUser->getName(),
                     'password' => bcrypt(str_random(10)),
+                    'ip' => 0.0.0.0
                 ]);
             }
 
