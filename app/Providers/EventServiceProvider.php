11f9d88545090d6d31437c18df3cac9307a97032
William Fisher <ashton0312@gmail.com>
2017-08-24 11:41:36 -0400

    Possible fix for class not found exception

--- app/Providers/EventServiceProvider.php
+++ app/Providers/EventServiceProvider.php
@@ -15,7 +15,7 @@ class EventServiceProvider extends ServiceProvider
     protected $listen = [
       \SocialiteProviders\Manager\SocialiteWasCalled::class => [
         'SocialiteProviders\Google\GoogleExtendSocialite@handle',
-        'vendor/socialiteproviders/nitrado/NitradoExtendSocialite.php@handle',
+        'SocialiteProviders/nitrado/NitradoExtendSocialite.php@handle',
       ],
     ];
 
