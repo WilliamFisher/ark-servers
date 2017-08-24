210d5c4fe499166495498d9a94d9e86e0822f31b
William Fisher <ashton0312@gmail.com>
2017-08-24 11:23:40 -0400

    Implement Nitrado provider

--- app/Providers/EventServiceProvider.php
+++ app/Providers/EventServiceProvider.php
@@ -15,6 +15,7 @@ class EventServiceProvider extends ServiceProvider
     protected $listen = [
       \SocialiteProviders\Manager\SocialiteWasCalled::class => [
         'SocialiteProviders\Google\GoogleExtendSocialite@handle',
+        'SocailiteProviders\Nitrado\NitradoExtendSocialite@handle',
       ],
     ];
 
