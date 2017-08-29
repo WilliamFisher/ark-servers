8e3db86232db8947b26d4df1c30b9de363c9ac83
William Fisher <ashton0312@gmail.com>
2017-08-28 23:41:34 -0400

    Made it more clear "name" should be server psn or gt

--- resources/views/server/create.blade.php
+++ resources/views/server/create.blade.php
@@ -22,7 +22,7 @@
             {{ csrf_field() }}
             <div class="form-group">
 
-              <label for="name" class="col-md-4 control-label">Name</label>
+              <label for="name" class="col-md-4 control-label">GT or PSN</label>
 
               <div class="col-md-6">
                 <input id="name" type="text" class="form-control" placeholder="Gamertag or PSN name" name="name" value="{{ old('name') }}" maxlength="16" required autofocus>
